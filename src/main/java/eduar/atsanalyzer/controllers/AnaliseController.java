b240589f90cd2415eba15aa5a0c1444dcff6cd76
eduardo ciudad figueredo <eduardociudadfigueredo@gmail.com>
2026-06-27 21:35:08 -0300

    feat: terminei o controller

--- src/main/java/eduar/atsanalyzer/controllers/AnaliseController.java
+++ src/main/java/eduar/atsanalyzer/controllers/AnaliseController.java
@@ -1,8 +1,17 @@
 package eduar.atsanalyzer.controllers;
 
+import eduar.atsanalyzer.dtos.request.AnaliseRequest;
+import eduar.atsanalyzer.dtos.response.AnaliseResponse;
+import eduar.atsanalyzer.services.AnaliseService;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestPart;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
 
 @RestController
 @RequiredArgsConstructor
@@ -10,4 +19,12 @@ import org.springframework.web.bind.annotation.RestController;
 public class AnaliseController {
 
     private final AnaliseService analiseService;
+
+    @PostMapping(consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
+    public ResponseEntity<AnaliseResponse> analisar(
+            @RequestPart("curriculo") MultipartFile curriculo,
+            @RequestPart("vaga") @Valid AnaliseRequest request) {
+
+        return ResponseEntity.ok(analiseService.analisar(curriculo, request));
+    }
 }
