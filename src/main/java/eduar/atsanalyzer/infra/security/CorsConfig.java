b83bb0c8fc8774d3516806e3f0b5d59341d5794c
eduardo ciudad figueredo <eduardociudadfigueredo@gmail.com>
2026-06-27 22:31:10 -0300

    feat: create cors config

---
+++ src/main/java/eduar/atsanalyzer/infra/security/CorsConfig.java
@@ -0,0 +1,29 @@
+package eduar.atsanalyzer.infra.security;
+
+
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
+
+import java.util.List;
+
+@Configuration
+public class CorsConfig {
+
+    @Bean
+    public CorsFilter corsFilter() {
+        CorsConfiguration config = new CorsConfiguration();
+        config.setAllowedOrigins(List.of("http://127.0.0.1:5500", "http://localhost:5500"));
+        config.setAllowedMethods(List.of("POST", "GET", "OPTIONS"));
+        config.setAllowedHeaders(List.of("*"));
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", config);
+
+        return new CorsFilter(source);
+    }
+}
