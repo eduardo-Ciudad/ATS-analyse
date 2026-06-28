1e586d36b2fd79804710b037e06930ac638f5246
eduardo ciudad figueredo <eduardociudadfigueredo@gmail.com>
2026-06-27 21:19:37 -0300

    feat: terminei PdfExtractorService

---
+++ src/main/java/eduar/atsanalyzer/infra/ia/PromptBuilder.java
@@ -0,0 +1,4 @@
+package eduar.atsanalyzer.infra.ia;
+
+public class PromptBuilder {
+}
--- src/main/java/eduar/atsanalyzer/services/PdfExtractorService.java
+++ src/main/java/eduar/atsanalyzer/services/PdfExtractorService.java
@@ -1,7 +1,26 @@
 package eduar.atsanalyzer.services;
 
 import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+import org.apache.pdfbox.pdmodel.PDDocument;
+import org.apache.pdfbox.text.PDFTextStripper;
+import java.io.IOException;
 
 @Service
 public class PdfExtractorService {
+    public String extrairTexto(MultipartFile file) {
+        try (PDDocument document = PDDocument.load(file.getInputStream())) {
+            PDFTextStripper stripper = new PDFTextStripper();
+            String texto = stripper.getText(document);
+
+            if (texto.isBlank()) {
+                throw new RuntimeException("O PDF não contém texto extraível");
+            }
+
+            return texto;
+
+        } catch (IOException e) {
+            throw new RuntimeException("Erro ao processar o arquivo PDF", e);
+        }
+    }
 }
