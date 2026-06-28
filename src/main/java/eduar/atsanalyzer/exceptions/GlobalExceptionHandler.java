bf55436e6480a8b4b5743c2097bf27680b97ca86
eduardo ciudad figueredo <eduardociudadfigueredo@gmail.com>
2026-06-27 21:47:21 -0300

    feat: crate globlaexceptionHandler

--- src/main/java/eduar/atsanalyzer/exceptions/GlobalExceptionHandler.java
+++ src/main/java/eduar/atsanalyzer/exceptions/GlobalExceptionHandler.java
@@ -1,4 +1,24 @@
 package eduar.atsanalyzer.exceptions;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.util.Map;
+
+@RestControllerAdvice
 public class GlobalExceptionHandler {
+    @ExceptionHandler(PdfProcessingException.class)
+    public ResponseEntity<Map<String, String>> handlerPdf(PdfProcessingException e) {
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST)
+                .body(Map.of("erro", e.getMessage()));
+    }
+
+    @ExceptionHandler(AnaliseException.class)
+    public ResponseEntity<Map<String, String>> handleAnalise(AnaliseException e) {
+        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
+                .body(Map.of("erro", e.getMessage()));
+    }
+
 }
